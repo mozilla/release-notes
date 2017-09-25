b4fe54ad2e1448ab8fc395ba371be7551a549541
Paul McLanahan <pmac@mozilla.com>
2017-09-25 13:12:08 -0400

    Add ETag support

--- update_releases.py
+++ update_releases.py
@@ -10,12 +10,31 @@ import requests
 
 RELEASES_URL = os.getenv('RELEASES_URL', 'https://nucleus.mozilla.org/rna/all-releases.json')
 OUTPUT_DIR = Path(__file__).with_name('releases')
+ETAG_FILE = OUTPUT_DIR.joinpath('.latest-update-etag')
+
+
+def get_request_headers():
+    headers = {}
+    if ETAG_FILE.exists():
+        with ETAG_FILE.open() as fh:
+            headers['If-None-Match'] = fh.read()
+
+    return headers
 
 
 def get_release_data():
-    resp = requests.get(RELEASES_URL, timeout=5)
+    etag = None
+    headers = get_request_headers()
+    resp = requests.get(RELEASES_URL, headers=headers, timeout=10)
     resp.raise_for_status()
-    return resp.json()
+    if resp.status_code == 304:
+        # no updates, etags match
+        return None, None
+
+    if 'etag' in resp.headers:
+        etag = resp.headers['etag']
+
+    return resp.json(), etag
 
 
 def setup():
@@ -23,21 +42,28 @@ def setup():
     OUTPUT_DIR.mkdir()
 
 
-def write_files(release_data):
+def write_files(release_data, etag=None):
     for release in release_data:
         with OUTPUT_DIR.joinpath('%s.json' % release['slug']).open('w') as fp:
             json.dump(release, fp, indent=2, sort_keys=True)
+
+    if etag:
+        with ETAG_FILE.open('w') as fh:
+            fh.write(etag)
 
     print('Wrote {} files'.format(len(release_data)))
 
 
 def main():
     try:
-        data = get_release_data()
+        data, etag = get_release_data()
     except requests.RequestException as e:
         return 'ERROR: %s' % e
-    setup()
-    write_files(data)
+    if data:
+        setup()
+        write_files(data, etag)
+    else:
+        print('Releases are up to date. No update required.')
 
 
 if __name__ == '__main__':
