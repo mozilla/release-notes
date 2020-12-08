5a092e16ebd45755298a69e9438f3599f2fd014f
Paul McLanahan <pmac@mozilla.com>
2020-12-08 17:01:16 -0500

    Use last-modified header instead of etag for updates
    See https://github.com/mozilla/nucleus/pull/347

--- update_releases.py
+++ update_releases.py
@@ -10,62 +10,67 @@ import requests
 
 RELEASES_URL = os.getenv('RELEASES_URL', 'https://nucleus.mozilla.org/rna/all-releases.json')
 OUTPUT_DIR = Path(__file__).with_name('releases')
-ETAG_FILE = OUTPUT_DIR.joinpath('.latest-update-etag')
+LAST_MODIFIED = OUTPUT_DIR.joinpath('.last-modified')
 
 
 def get_request_headers():
     headers = {}
-    if ETAG_FILE.exists():
-        with ETAG_FILE.open() as fh:
-            headers['If-None-Match'] = fh.read()
+    if LAST_MODIFIED.exists():
+        with LAST_MODIFIED.open() as fh:
+            headers['If-Modified-Since'] = fh.read()
 
     return headers
 
 
-def get_release_data():
-    etag = None
-    headers = get_request_headers()
-    resp = requests.get(RELEASES_URL, headers=headers, timeout=30)
+def get_release_data(everything):
+    last_modified = None
+    if everything:
+        resp = requests.get(RELEASES_URL, {'all': 'true'}, timeout=60)
+    else:
+        headers = get_request_headers()
+        resp = requests.get(RELEASES_URL, headers=headers, timeout=30)
+
     resp.raise_for_status()
     if resp.status_code == 304:
-        # no updates, etags match
+        # no updates, nothing modified since last_modified
         return None, None
 
-    if 'etag' in resp.headers:
-        etag = resp.headers['etag']
+    if 'last-modified' in resp.headers:
+        last_modified = resp.headers['last-modified']
 
-    return resp.json(), etag
+    return resp.json(), last_modified
 
 
-def setup():
+def cleanup():
     rmtree(str(OUTPUT_DIR), ignore_errors=True)
     OUTPUT_DIR.mkdir()
 
 
-def write_files(release_data, etag=None):
+def write_files(release_data, last_modified=None):
     for release in release_data:
         with OUTPUT_DIR.joinpath('%s.json' % release['slug']).open('w') as fp:
             json.dump(release, fp, indent=2, sort_keys=True)
 
-    if etag:
-        with ETAG_FILE.open('w') as fh:
-            fh.write(etag)
+    if last_modified:
+        with LAST_MODIFIED.open('w') as fh:
+            fh.write(last_modified)
 
     print('Wrote {} files'.format(len(release_data)))
 
 
-def main():
+def main(everything):
     try:
-        data, etag = get_release_data()
+        data, last_modified = get_release_data(everything)
     except requests.RequestException as e:
         return 'ERROR: %s' % e
     if data:
-        # skip this for now
-        # setup()
-        write_files(data, etag)
+        if everything:
+            cleanup()
+
+        write_files(data, last_modified)
     else:
         print('Releases are up to date. No update required.')
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    sys.exit(main(sys.argv[-1] == '--all'))
